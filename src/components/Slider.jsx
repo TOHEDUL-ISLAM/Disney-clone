d4bf85260e1073cd59f56efdc613d05c3a40c8dc
nirzon32 <tohedulislamnirzon@gmail.com>
2024-11-01 19:32:53 +0600

    added new slider .. added width fixed during slide

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import GlobalApi from "../services/GlobalApi";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+import Carousel from "./carousel";
 
 const Slider = () => {
   const [movieList, setMovieList] = useState([]);
@@ -18,30 +19,23 @@ const Slider = () => {
       console.log(res.data.results);
     });
   };
-  const sliderRight = (element) => {
-        element.scrollLeft += screenWidth-110
-    
-  };
-  const sliderLeft = (element) => {
-    element.scrollLeft -= screenWidth-110
-
-};
+ 
   return (
     <div>
-      <div className="hidden md:block">
-        <HiChevronLeft onClick={()=> sliderLeft(elementRef.current)} className="text-white text-[30px] absolute my-[250px] mx-5"></HiChevronLeft>
-        <HiChevronRight onClick={()=> sliderRight(elementRef.current)} className=" text-white text-[30px] absolute my-[250px] mx-5 right-0"></HiChevronRight>
-      </div>
+
       <div className=" flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth" ref={elementRef}>
-        {movieList.map((movie) => movie.adult===false&&(
+      <Carousel autoSlide autoSlideInterval={5000}>
+      {movieList.map((movie) =>
+        !movie.adult ? (
           <img
+            key={movie.id} // Assuming movie.id is unique
             src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
             alt={movie.title}
-            className=" border-1  min-w-full  md:h-[500px] object-cover
-            object-left-top mr-5 rounded-md hover:border-[4px]
-            border-gray-600 transition-all duration-100 ease-in"
+            className="border-1 min-w-full md:h-[500px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-600 transition-all duration-100 ease-in"
           />
-        ))}
+        ) : null
+      )}
+    </Carousel>
       </div>
     </div>
   );
